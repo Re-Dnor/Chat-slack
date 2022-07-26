8061802eb93d6412a833ed470fdee7f9752db904
Re-Dnor <MokshinRE@yandex.ru>
2022-07-26 18:51:42 +0500

    feat: change starting logic

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />,
-);
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import init from './init.jsx';
+
+const startChat = async () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  const vdom = await init();
+
+  root.render(<React.StrictMode>{vdom}</React.StrictMode>);
+};
+
+startChat();
