a1cd02714f9a228e13a3c3fb2fba0341269fcf83
Re-Dnor <MokshinRE@yandex.ru>
2022-07-27 18:28:44 +0500

    feat: add new route "signup"

--- frontend/src/Components/App.jsx
+++ frontend/src/Components/App.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-
-import Home from './Home.jsx';
+import Chat from './Chat.jsx';
 import Login from './Login.jsx';
 import NotFoundPage from './NotFoundPage.jsx';
+import Navbar from './Navbar.jsx';
+import useAuth from '../hooks/useAuth.js';
+import SignUp from './SignUp.jsx';
 
 function App() {
+  const { authStatus } = useAuth();
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={authStatus ? <Chat /> : <Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFoundPage />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </>
   );
 }
 
