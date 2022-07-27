4f81db9447e994291676ed09a3393355bef3e82d
Re-Dnor <MokshinRE@yandex.ru>
2022-07-27 19:16:39 +0500

    add corrections

--- frontend/src/context/AuthContextProvider.jsx
+++ frontend/src/context/AuthContextProvider.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import AuthContext from './AuthContext';
+import AuthContext from './AuthContext.js';
 
 function AuthContextProvider({ children }) {
   // const getUser = () => localStorage.getItem('username');
