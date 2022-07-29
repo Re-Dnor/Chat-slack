32788ec0ec9322a13d6e4a72226fc0247e1b88ef
Re-Dnor <MokshinRE@yandex.ru>
2022-07-29 18:54:03 +0500

    feat: use provider (redux-toolkit)

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import AuthContextProvider from './context/AuthContextProvider.jsx';
+import store from './store.js';
 
 import languages from './locales/index.js';
 import App from './Components/App.jsx';
@@ -19,11 +21,13 @@ export default async () => {
 
   const vdom = (
     <I18nextProvider i18n={i18n}>
-      <BrowserRouter>
-        <AuthContextProvider>
-          <App />
-        </AuthContextProvider>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <AuthContextProvider>
+            <App />
+          </AuthContextProvider>
+        </BrowserRouter>
+      </Provider>
     </I18nextProvider>
   );
 
