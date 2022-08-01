ec582a81d9789f723f01731e03d97b134b73bc9e
Re-Dnor <MokshinRE@yandex.ru>
2022-08-01 22:56:46 +0500

    refactor: change directory

---
+++ frontend/src/store/features/chats-slice.js
@@ -0,0 +1,54 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+import routes from '../../routes.js';
+
+export const loadChats = createAsyncThunk('@@chats/load-chats', (configAuthorization) => {
+  const { mainPagePath } = routes;
+  return axios.get(mainPagePath(), configAuthorization);
+});
+
+const initialState = {
+  channels: [],
+  messages: [],
+  currentChannel: null,
+  status: 'idle',
+  error: null,
+};
+
+const chatsSlice = createSlice({
+  name: '@@chats',
+  initialState,
+  reducers: {
+    setActiveChannel: (state, action) => ({
+      ...state,
+      currentChannel: action.payload,
+    }),
+    addMessage: (state, action) => {
+      state.messages.push(action.payload);
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loadChats.pending, (state) => {
+        state.status = 'loading';
+        state.error = null;
+      })
+      .addCase(loadChats.fulfilled, (state, action) => {
+        const { channels, messages, currentChannelId } = action.payload.data;
+        state.status = 'received';
+        state.channels = channels;
+        state.messages = messages;
+        state.currentChannel = currentChannelId;
+        state.error = null;
+      })
+      .addCase(loadChats.rejected, (state, action) => {
+        state.status = 'rejected';
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const { setActiveChannel, addMessage } = chatsSlice.actions;
+const chatsReducer = chatsSlice.reducer;
+export default chatsReducer;
