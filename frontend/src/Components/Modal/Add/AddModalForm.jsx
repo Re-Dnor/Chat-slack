1fc8035345e18a0bf5d18a5b9dbc8ec6e40a11d4
Re-Dnor <MokshinRE@yandex.ru>
2022-08-03 18:07:25 +0500

    feat: add modal windows

---
+++ frontend/src/Components/Modal/Add/AddModalForm.jsx
@@ -0,0 +1,75 @@
+import React, { useRef, useEffect } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useFormik } from 'formik';
+import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import { toast } from 'react-toastify';
+
+import validationForm from '../../../helpers/validation.js';
+import { useSocket } from '../../../hooks/useAuth.js';
+
+function AddModalForm({ setShowModal }) {
+  const textInput = useRef(null);
+  const { t } = useTranslation();
+  const { channels } = useSelector((state) => state.chats);
+  const channelsName = channels.map((channel) => channel.name);
+  const socket = useSocket();
+  const { addChannelForm } = validationForm();
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+    },
+    validationSchema: addChannelForm(channelsName),
+    onSubmit: (values) => {
+      socket.newChannel(values, (response) => {
+        if (response.status === 'ok') {
+          setShowModal(false);
+          toast.success(t('toast.add'));
+        }
+      });
+    },
+  });
+
+  useEffect(() => {
+    textInput.current.focus();
+  }, []);
+
+  return (
+    <Form onSubmit={formik.handleSubmit}>
+      <Form.Group>
+        <Form.Control
+          name="name"
+          data-testid="add-channel"
+          className="mb-2"
+          onChange={formik.handleChange}
+          ref={textInput}
+          disabled={formik.isSubmitting}
+          isInvalid={formik.errors.name}
+        />
+        <Form.Control.Feedback type="invalid">
+          {t(formik.errors.name)}
+        </Form.Control.Feedback>
+        <div className="d-flex justify-content-end">
+          <Button
+            onClick={handleClose}
+            type="button"
+            name={t('modals.cancel')}
+            className="me-2 btn btn-secondary"
+          >
+            {t('modals.cancel')}
+          </Button>
+          <Button type="submit" name={t('modals.send')} className="btn btn-primary">
+            {t('modals.send')}
+          </Button>
+        </div>
+      </Form.Group>
+    </Form>
+  );
+}
+
+export default AddModalForm;
