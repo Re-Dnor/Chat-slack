abdd2597f916381c43f57f49ffc48294b3f63a45
Re-Dnor <MokshinRE@yandex.ru>
2022-08-04 18:52:20 +0500

    feat: change structure

---
+++ frontend/src/Components/Pages/Chat.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Container, Row } from 'react-bootstrap';
+
+import { useAuth } from '../../hooks/useAuth';
+import { loadChats } from '../../store/features/chats-slice.js';
+import Channels from '../Channels.jsx';
+import Messages from '../Messages.jsx';
+import Loader from '../Loader.jsx';
+
+function Chat() {
+  const dispatch = useDispatch();
+  const { status, error } = useSelector((state) => state.chats);
+
+  const { getHeader } = useAuth();
+  useEffect(() => {
+    dispatch(loadChats(getHeader));
+  }, []);
+
+  return (
+    <Container className="container h-100 my-4 overflow-hidden rounded shadow">
+      {error && <h2>{error}</h2>}
+      {status === 'loading' && <Loader />}
+      {status === 'received' && (
+        <Row className="h-100 bg-white flex-md-row">
+          <Channels />
+          <Messages />
+        </Row>
+      )}
+    </Container>
+  );
+}
+export default Chat;
