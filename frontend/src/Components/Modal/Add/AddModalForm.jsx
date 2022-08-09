99a1aea097916b78c5f947d025698b48b0287584
Re-Dnor <MokshinRE@yandex.ru>
2022-08-09 18:10:47 +0500

    fix: add corrections

--- frontend/src/Components/Modal/Add/AddModalForm.jsx
+++ frontend/src/Components/Modal/Add/AddModalForm.jsx
@@ -42,7 +42,7 @@ function AddModalForm({ setShowModal }) {
   return (
     <Form onSubmit={formik.handleSubmit}>
       <Form.Group>
-        <FloatingLabel label={t('modals.nameChannel')} htmlFor='AddedForm'>
+        <FloatingLabel label={t('modals.nameChannel')} controlId="AddedForm">
           <Form.Control
             name="channelName"
             id='AddedForm'
